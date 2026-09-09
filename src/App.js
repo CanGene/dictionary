c795885e8ed1791662d2ef9fbb8f0df3d81119b7
Candace Genesis <candace.genesis@gmail.com>
2026-09-09 15:45:28 -0700

    Set up search field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <div className="container">
+        <header className="App-header">
+          <h1 className="text-center">Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://github.com/CanGene"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Candace Genesis
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
