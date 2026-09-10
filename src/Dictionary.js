fbfa3730eef6706af82e1428d92716e790e0badc
Candace Genesis <candace.genesis@gmail.com>
2026-09-10 11:07:40 -0700

    Rendered definitions of words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -20,6 +22,7 @@ export default function Dictionary() {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
@@ -30,6 +33,8 @@ export default function Dictionary() {
           placeholder="Search for a word..."
         />
       </form>
+
+      <Results results={results} />
     </div>
   );
 }
